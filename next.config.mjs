5f2e68749bfd37190a3b26cf4b83f42d30f72911
sachin9272 <sachinsingh9272@gmail.com>
2026-07-17 10:52:12 +0530

    Updated Next js config

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,28 @@
 const nextConfig = {
   reactCompiler: true,
   images: {
-    qualities: [75, 80, 85, 90], 
+    qualities: [75, 80, 85, 90],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'luxurydata.s3.eu-north-1.amazonaws.com',
+        protocol: "https",
+        hostname: "luxurydata.s3.eu-north-1.amazonaws.com",
+        pathname: "/**",
       },
     ],
-    unoptimized: true,
+  unoptimized: false,
+  formats: ["image/avif", "image/webp"],
   },
   experimental: {
     serverActions: {
-      allowedOrigins: ['hs23fwgh-3000.inc1.devtunnels.ms', 'localhost:3000', '*.devtunnels.ms']
-    }
-  }
+      allowedOrigins: [
+        "localhost:3000",
+        "*.devtunnels.ms",
+      ],
+    },
+  },
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
