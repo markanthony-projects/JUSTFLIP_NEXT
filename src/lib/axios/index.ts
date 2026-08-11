bbda751e934cb06b9b50b1d108ec77e3003194df
sachin9272 <sachinsingh9272@gmail.com>
2026-08-11 14:33:08 +0530

    Fixed Prefooter Redirection

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -7,6 +7,7 @@ interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
+    timeout: 10000,
 });
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
