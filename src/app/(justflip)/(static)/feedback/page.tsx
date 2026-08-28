2e25b6b1868dbf9c16dba2c6ba7e6de18ac8bbfb
sachin9272 <sachinsingh9272@gmail.com>
2026-08-28 10:33:03 +0530

    Fixed Accessibility

--- src/app/(justflip)/(static)/feedback/page.tsx
+++ src/app/(justflip)/(static)/feedback/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import FeedbackClient from './FeedbackClient';
 
+export const metadata = {
+  title: "Feedback | JustFlip",
+  description: "Share your feedback, suggestions, or experience to help us improve JustFlip.",
+};
+
 const page = () => {
   return (
     <div>
